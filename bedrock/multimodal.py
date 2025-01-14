f29d8ab4552f07aacaca8fa035b4aa30aa79d930
Florian Clanet <Flolight@users.noreply.github.com>
2025-01-14 17:12:29 +0100

    Update multimodal.py

--- bedrock/multimodal.py
+++ bedrock/multimodal.py
@@ -27,3 +27,33 @@ response = bedrock_client.converse(
 )
 
 print(response)
+
+###### Simple test with prompt ######
+
+with open("./images/test.png", "rb") as image_file:
+  binary_data = image_file.read()
+
+message = {
+  "role": "user",
+  "content": [
+    {"text": "Describe steps to prevent the scenario pictured in this image."},
+    {
+      "image": {
+        "format": 'png',
+        "source": {
+          "bytes": binary_data
+        }
+      }
+    }
+  ]
+}
+
+bedrock_client = boto3.client(service_name='bedrock-runtime', region_name="us-west-2")
+model_id="anthropic.claude-3-5-sonnet-20241022-v2:0"
+
+response = bedrock_client.converse(
+  modelId=modelId,
+  messages=[messages],
+)
+
+print(response)
