966a467a4ec541095d9a995cc5552a3cf6602b89
Florian Clanet <Flolight@users.noreply.github.com>
2025-01-14 17:08:39 +0100

    Update multimodal.py

--- bedrock/multimodal.py
+++ bedrock/multimodal.py
@@ -1,5 +1,6 @@
 import boto3
 
+###### Simple test ######
 with open("./images/test.png", "rb") as image_file:
   binary_data = image_file.read()
 
@@ -16,3 +17,13 @@ message = {
     }
   ]
 }
+
+bedrock_client = boto3.client(service_name='bedrock-runtime', region_name="us-west-2")
+model_id="anthropic.claude-3-5-sonnet-20241022-v2:0"
+
+response = bedrock_client.converse(
+  modelId=modelId,
+  messages=[messages],
+)
+
+print(response)
