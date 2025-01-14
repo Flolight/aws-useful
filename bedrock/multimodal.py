6e598f924b65f187c64ceb5095cbc3ee98eb3d63
Florian Clanet <Flolight@users.noreply.github.com>
2025-01-14 17:19:31 +0100

    Update multimodal.py

--- bedrock/multimodal.py
+++ bedrock/multimodal.py
@@ -57,3 +57,49 @@ response = bedrock_client.converse(
 )
 
 print(response)
+
+
+#########################
+
+import mimetypes
+
+def create_image_message(image_path):
+    # Open the image file in "read binary" mode
+    with open(image_path, "rb") as image_file:
+        # Read the contents of the image as a bytes object
+        binary_data = image_file.read()
+
+    # Get the MIME type of the image based on its file extension
+    mime_type, _ = mimetypes.guess_type(image_path)
+
+    sub_type = mime_type.split("/")[-1]
+
+    # Create the image block
+    image_block = {
+        "image": {
+            "format": sub_type,
+            "source": {
+                "bytes": binary_data
+            }
+        }
+    }
+
+    return image_block
+
+message = {
+  "role": "user",
+  "content": [
+    {"text": "Identify the subject of each of these images, using a simple word for each image."},
+    create_image_message("./images/test1.png"),
+    create_image_message("./images/test2.png"),
+  ]
+}
+response = bedrock_client.converse(
+  modelId=modelId,
+  messages=[messages],
+)
+
+print(response)
+
+
+
